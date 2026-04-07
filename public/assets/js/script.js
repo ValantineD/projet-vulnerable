6d1a8367f0ab2dd3490256b9f1934e1e0856dbdd
ValantineD <v.dagany@gmail.com>
2026-04-07 16:15:24 +0200

    add cookie script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,10 +6,14 @@ console.log('hello');
  * */
 
 // <script>
-//
+
 // const p = document.querySelectorAll('p');
 //
 // for (const e of p) {
 //     e.style.color = 'red';
 // }
 // </script>
+
+document.cookie = "username=Deschger;";
+document.cookie = "email=email123@gxss.fr;";
+document.cookie = "password=password;";
